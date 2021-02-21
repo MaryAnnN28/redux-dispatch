ae9ef49e3e50ae91a0241624e05936dfe5bfbb9d
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-02-21 16:04:03 -0500

    commit tree fb362d10fc86f9a325a8c9e0049997ef7db1baee

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,24 @@
-function changeState(state, action){
+// function changeState(state, action){
+//   switch (action.type) {
+//     case 'INCREASE_COUNT':
+//       return {count: state.count + 1}
+//     default:
+//       return state;
+//   }
+// }
+
+let state = { count: 0 }; 
+
+function changeState(state, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
-      return {count: state.count + 1}
+      return { count: state.count + 1 } 
     default:
       return state;
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
-
-changeState(state, action)
+function dispatch(action) {
+  state = changeState(state, action)
+  return state
+}
